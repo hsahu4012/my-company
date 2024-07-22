e8e09bd44a94ee3392b847d29f245952a20d5d3d
hsahu4012 <hrudayanandasahu3@gmail.com>
2024-07-22 07:55:16 +0530

    margin-fix

--- src/app/_components/OurTeam.jsx
+++ src/app/_components/OurTeam.jsx
@@ -5,11 +5,11 @@ const OurTeam = () => {
 
     return (
         <div className="container mx-auto gap-5 py-8 bg-gray-100">
-            <div className="flex flex-col md:flex-row justify-between items-center gap-10 mb-16">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-10 mb-8">
                 <div className="md:w-1/2">
-                    <h2 className="text-3xl font-bold mb-4">Our team</h2>
-                    <p className="mb-4">Thousand is a full-cycle digital production company with its own product analytics, design, web and mobile development.</p>
-                    <div className="flex justify-between mb-4">
+                    <h2 className="text-3xl font-bold mb-10">Our team</h2>
+                    <p className="mb-10">Thousand is a full-cycle digital production company with its own product analytics, design, web and mobile development.</p>
+                    <div className="flex justify-between mb-15">
                         <div>
                             <p className="text-3xl font-bold">28</p>
                             <p>team members</p>
@@ -23,7 +23,7 @@ const OurTeam = () => {
                             <p>in IT sphere</p>
                         </div>
                     </div>
-                    <p className="text-sm">All the necessary specialists - from a designer to a tester - are on our staff. We hire the best specialists in the market. It&apos;s expensive but worth it!</p>
+                    <p className="text-sm py-10">All the necessary specialists - from a designer to a tester - are on our staff. We hire the best specialists in the market. It&apos;s expensive but worth it!</p>
                 </div>
                 <div className="md:w-1/2 mt-8 md:mt-0">
                     <Image src="/images/ourteam.png" alt="Our Team" className="rounded-lg shadow-lg" height={400} width={600} />
